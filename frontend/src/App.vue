1170fddeb943457e16b4f64fed7bf5333685739c
Víctor Gutierrez <victor4guiar89@gmail.com>
2023-10-25 21:20:45 -0300

    asdf

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<!-- importo las fuentes -->
+<!-- importo las fuent es -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
